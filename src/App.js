3e24396c296b6a884929b74bfdd9b37ba141e73c
BDedafoe <bmd6260@gmail.com>
2022-09-13 18:16:55 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
     if(data) {
       return (
         <Suspense fallback={<Spinner/>}>
-          <Gallery data={data}/>
+          <Gallery />
         </Suspense>
       )
     }
